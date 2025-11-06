c82273e6c5dcbd52f5935b0f6f940f03723c7289
Stepan Samutichev <stepan.samutichev@gmail.com>
2025-11-06 14:41:49 +0300

    Initial commit
    Project initialized with IntelliJ IDEA CE:
    • Java;
    • Oracle GraalVM 25;
    • Gradle 9.2.0.

---
+++ src/main/java/org/team2aston/Main.java
@@ -0,0 +1,11 @@
+package org.team2aston;
+
+public class Main {
+    static void main() {
+        IO.println("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            IO.println("i = " + i);
+        }
+    }
+}
