e9114348b7ed92e24f8eb49b5cde8926e101d014
TruckerVyacheslav <152480177+TruckerVyacheslav@users.noreply.github.com>
2025-11-12 14:29:07 +0300

    Delete src/main/java/org/team2aston/Sorting directory
    Случайно закомитились файлы сортинга, удалил для ясности веток
    
    Signed-off-by: TruckerVyacheslav <152480177+TruckerVyacheslav@users.noreply.github.com>

--- src/main/java/org/team2aston/Sorting/ClassicSortStrategy.java
+++
@@ -1,8 +0,0 @@
-package org.team2aston.Sorting;
-
-import java.util.Comparator;
-import java.util.List;
-
-public interface ClassicSortStrategy<T> {                         //интерфейс для классической сортировки
-    void sort(List<T> list, Comparator<? super T> comparator);
-}
--- src/main/java/org/team2aston/Sorting/SpecialSortStrategy.java
+++
@@ -1,9 +0,0 @@
-package org.team2aston.Sorting;
-
-import java.util.Comparator;
-import java.util.List;
-import java.util.function.ToIntFunction;
-
-public interface SpecialSortStrategy<T> {               //интерфейс для сортировки по условиям доп. задания 1
-    void sort(List<T> list, Comparator<? super T> comparator, ToIntFunction<T> nums);
-}
